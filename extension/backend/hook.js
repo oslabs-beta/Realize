1c6b85f19e6b81774a1634ca56e20b6094c4949e
Harry Clifford <harryclifford@Harrys-MacBook-Pro.local>
2020-06-02 16:27:25 -0400

    hook injected from content-script

--- extension/backend/hook.js
+++ extension/backend/hook.js
@@ -1,17 +1,25 @@
+/* eslint-disable no-param-reassign */
 /* eslint-disable no-undef */
 /* eslint-disable no-use-before-define */
 /* eslint-disable func-names */
 /* eslint-disable no-underscore-dangle */
 function hook() {
+  console.log('hooked');
   const devTools = window.__REACT_DEVTOOLS_GLOBAL_HOOK__;
+
+  if (!devTools) {
+    console.log('this page does not use React');
+    return;
+  }
+
   devTools.onCommitFiberRoot = (function (original) {
     return function (...args) {
       const fiberDOM = args[1];
       const rootNode = fiberDOM.current.stateNode.current;
       const arr = [];
       recurse(rootNode.child, arr);
-      console.log(arr);
-      //   sendToServer(arr);
+      console.log('fibertree: ', arr);
+      // sendToServer(arr);
       return original(...args);
     };
   })(devTools.onCommitFiberRoot);
@@ -27,22 +35,51 @@ function sendToServer(arr) {
   });
 }
 
+function getProps(props) {
+  const cleanProps = {};
+  Object.keys(props).forEach((prop) => {
+    cleanProps[prop] = props[prop];
+  });
+
+  return cleanProps;
+}
+
 // recursion for state linked list
 function getState(stateNode, arr) {
   //   arr.push('something');
   arr.push(stateNode.memoizedState);
   // && stateNode.next.memoizedState.tag !== 5
-  if (stateNode.next)
+  if (stateNode.next && stateNode.next.memoizedState.tag !== 5)
     // ^^^
     // DEFINITELY CHECK THIS OUT
     getState(stateNode.next, arr);
+}
+
+// get type of state
+function getStateType(node, component) {
+  // has own state
+  const stateful = !!component.state;
+
+  // receives props from parent
+  const receiving = !!component.props;
+
+  // has children that receive props
+  const sending =
+    component.children && component.children.some((child) => child.props);
+
+  if (!stateful && !receiving && !sending) return -1;
+
+  return {
+    stateful,
+    receiving,
+    sending,
+  };
 }
 
 // TODO: write conditions and separate funcs for class and functional components
 function recurse(node, parentArr) {
   const component = {
     name: '',
-    props: null,
     // node,
   };
 
@@ -73,22 +110,35 @@ function recurse(node, parentArr) {
   } else if (node.memoizedState) component.state = node.memoizedState;
 
   // get props
-  if (node.memoizedProps) component.props = node.memoizedProps;
+  if (node.memoizedProps && Object.keys(node.memoizedProps).length > 0)
+    component.props = getProps(node.memoizedProps);
 
   // get hooks
   if (node._debugHookTypes) component.hooks = node._debugHookTypes;
 
-  //   if (component.name === 'App') component.state = null;
+  if (component.name === 'App') component.state = null;
 
+  // insert component into parent's children array
   parentArr.push(component);
+
+  // get children/ siblings
   if (node.child) {
     component.children = [];
     recurse(node.child, component.children);
   }
   if (node.sibling) recurse(node.sibling, parentArr);
 
+  // get state type
+  component.stateType = getStateType(node, component);
+  if (component.stateType === -1) delete component.stateType;
+
   // remove children arr if none added in recursion
   if (component.children.length === 0) delete component.children;
+
+  // remove state if App component
+  if (!component.stateType && component.state === null) delete component.state;
 }
 
-module.exports = hook;
+hook();
+
+// module.exports = hook;
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
-const ExtensionReloader  = require('webpack-extension-reloader');
+const ExtensionReloader = require('webpack-extension-reloader');
 
 module.exports = {
   entry: {
@@ -16,19 +16,36 @@ module.exports = {
     new CopyPlugin({
       patterns: [
         { from: 'extension/manifest.json', to: '../extension/manifest.json' },
-        { from: 'extension/backend/background.js', to: '../extension/background.js' },
-        { from: 'extension/devtools/devtools-root.html', to: '../extension/devtools-root.html' },
-        { from: 'extension/devtools/create-panel.js', to: '../extension/create-panel.js' },
-        { from: 'extension/devtools/panel/panel.html', to: '../extension/panel.html' },
+        {
+          from: 'extension/backend/background.js',
+          to: '../extension/background.js',
+        },
+        {
+          from: 'extension/devtools/devtools-root.html',
+          to: '../extension/devtools-root.html',
+        },
+        {
+          from: 'extension/devtools/create-panel.js',
+          to: '../extension/create-panel.js',
+        },
+        {
+          from: 'extension/devtools/panel/panel.html',
+          to: '../extension/panel.html',
+        },
+        { from: 'extension/backend/hook.js', to: '../extension/hook.js' },
+        {
+          from: 'extension/backend/content_script.js',
+          to: '../extension/content_script.js',
+        },
       ],
     }),
     new ExtensionReloader({
-      manifest: path.resolve(__dirname, "./extension/manifest.json"),
-      entries: { 
+      manifest: path.resolve(__dirname, './extension/manifest.json'),
+      entries: {
         bundle: 'bundle',
-        background: '`background'
-      }
-    })
+        background: '`background',
+      },
+    }),
   ],
   // devtool: 'cheap-module-source-map', // Needed as to stop Chrome eval errors when using dev server
 };

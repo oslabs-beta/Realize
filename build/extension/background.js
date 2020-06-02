7a3aa4b4210f53ef89903d15b3baf22a292309d5
Horatiu Mitrea <horatiumitrea@Horatius-MacBook-Air.local>
2020-06-02 17:19:59 -0400

    Hot module working

---
+++ build/extension/background.js
@@ -0,0 +1,20 @@
+
+function handleMessage(request, sender, sendResponse) {
+
+  // Check that the message is coming from the devtools panel and nowhere else
+  if (sender.url != chrome.runtime.getURL("panel.html")) {
+    console.log("Unknown sender");
+    return;
+  }
+
+  // Execute the script that is send from Devtools (This is just an example)
+  chrome.tabs.executeScript(
+    request.tabId,
+    {
+      code: request.script
+    }
+  );
+}
+
+// Listen for messages from devtools
+chrome.runtime.onMessage.addListener(handleMessage);

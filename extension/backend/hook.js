84789929ca06692e0754155e5a12449c8910b0bd
Harry Clifford <harryclifford@Harrys-MacBook-Pro.local>
2020-06-06 15:25:00 -0400

    testing added

--- extension/backend/hook.js
+++ extension/backend/hook.js
@@ -7,7 +7,7 @@
 // Importing the D3 array from panel.js
 
 // importing data example
-import {searchData} from '../devtools/panel/search-example.js'
+import { searchData } from '../devtools/panel/search-example.js';
 
 function hook() {
   const devTools = window.__REACT_DEVTOOLS_GLOBAL_HOOK__;
@@ -26,15 +26,15 @@ function hook() {
 
   devTools.onCommitFiberRoot = (function (original) {
     return function (...args) {
-      const fiberDOM = args[1]; 
+      const fiberDOM = args[1];
       const rootNode = fiberDOM.current.stateNode.current;
       const arr = [];
       recurse(rootNode.child, arr);
       //console.log('Search Data:', searchData);
 
-      // component name hardcoded 
-      let compName = 'App';
-      console.log('searchhhhh', findComp(searchData, compName));
+      // component name hardcoded
+      // let compName = 'App';
+      // console.log('searchhhhh', findComp(searchData, compName));
       sendToContentScript(arr[0]);
       return original(...args);
     };
@@ -91,7 +91,9 @@ function getState(stateNode, arr) {
       // clean elements of state arr if they are react components
       if (elem.$$typeof && typeof elem.$$typeof === 'symbol') {
         console.log('bad state here');
-        stateArr.push(`< ${elem.type.name} />`);
+        stateArr.push(`<${elem.type.name} />`);
+      } else {
+        stateArr.push(elem);
       }
     });
     arr.push(stateArr);

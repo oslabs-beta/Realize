0f3f7bc7b83f710c349e1ea21c6171f8d6db3656
Horatiu Mitrea <horatiumitrea@Horatius-MacBook-Air.local>
2020-06-12 14:16:14 -0400

    Refactoring of search file in TS

--- extension/devtools/panel/search.js
+++ extension/devtools/panel/search.js
@@ -1,52 +1,29 @@
-// Recursively go over the tree until we find the name of a component
-// function findComp(arr, name) {
-//   const result = [];
-
-//   // Iterate over the array(one we get from D3)
-//   arr.forEach((elem) => {
-//     if (elem.data && elem.data.name === name) {
-//       const { data, ...clone } = elem;
-//       result.push(clone);
-//     } else {
-//       return -1;
-//     }
-//   });
-
-//   return result;
-// }
-
-
-let result = document.querySelector('.result');
-// let arr = ['HTML', 'CSS', 'PHP', 'Javascript', 'Dart', 'Python', 'Swift', 'Java', 'C++','Go','SASS','C#','LISP','Perl', 'Ruby']
-
-  // make it all lowercase
- function autoComplete(arr, input) {
-  //  Grab all nodes use d3.selectall
-  // replicate filter using d3 method -> d3 object of filters
-    let allNodes = d3.selectAll('circle.node');
-    return allNodes.filter(e =>e.data.name.toLowerCase().includes(input.toLowerCase()));
- }
-
-function getValue(val){
-    
-     // if no value, have an empty page, 
-     if(!val){
-       result.innerHTML='';
-       return
-     }
-   
-     // search goes here 
-     let data = autoComplete(arr,val);
-    
-    
+"use strict";
+exports.__esModule = true;
+// It works with Common JS File
+var d3 = require('../../libraries/d3.min.js');
+var result = document.querySelector('.result');
+// make it all lowercase
+function autoComplete(input) {
+    //  Grab all nodes use d3.selectalcll
+    // replicate filter using d3 method -> d3 object of filters
+    var allNodes = d3.selectAll('circle.node');
+    return allNodes.filter(function (e) { return e.data.name.toLowerCase().includes(input.toLowerCase()); });
+}
+function getValue(val) {
+    // if no value, have an empty page, 
+    if (!val) {
+        result.innerHTML = '';
+        return;
+    }
+    // search goes here 
+    var data = autoComplete(val);
     // append list data
-     let res = '';
-     data.each(e=>{
-        res += '<li>'+e.data.name+'</li>';
-     })
-  
-     result.innerHTML = res;
- }
-
-export { getValue }
-// module.exports = getValue;
+    var res = '';
+    data.each(function (e) {
+        res += '<li>' + e.data.name + '</li>';
+    });
+    result.innerHTML = res;
+}
+// export { getValue }
+exports["default"] = getValue;

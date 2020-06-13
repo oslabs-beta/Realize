18b9e535f999aa7b05d07e4676583b3c4a2b7b16
Henry Black <blackhaj@gmail.com>
2020-06-13 13:28:31 -0500

    US spelling for panel name

--- extension/devtools/create-panel.js
+++ extension/devtools/create-panel.js
@@ -2,7 +2,7 @@
 // This creates the dev tools panel using the panel.html file as the template
 
 chrome.devtools.panels.create(
-  'Realise', // title
+  'Realize', // title
   '', // icon
   './panel.html' // content
 );

54aa95e2f43ef3e97b4c851ff0421891cbf7754f
Horatiu Mitrea <horatiumitrea@Horatius-MacBook-Air.local>
2020-06-13 15:22:01 -0400

    Added search listener in the ts file

--- extension/devtools/panel/search.ts
+++ extension/devtools/panel/search.ts
@@ -4,42 +4,52 @@ const d3 = require('../../libraries/d3.min.js')
 let result = document.querySelector('.result');
 
   // make it all lowercase
- function autoComplete(input) {
-  //  Grab all nodes use d3.selectalcll
-  // replicate filter using d3 method -> d3 object of filters
-     let allNodes = d3.selectAll('circle.node');
-     return allNodes.filter(e =>e.data.name.toLowerCase().includes(input.toLowerCase()));
-     
- }
+ 
 
-function getValue(val){
-    
-     // if no value, have an empty page, 
-     if(!val){
-       result.innerHTML='';
-       return
-     }
-   
-     // search goes here 
-     let data = autoComplete(val);
-    
-    // append list data
-     let res = '';
-     data.each(e=>{
-        res += '<li>'+e.data.name+'</li>';
-        console.log('hi');
-     })
+function addSearchListener(valuesArray) {
 
+  function autoComplete(input) {
+    //  Grab all nodes use d3.selectalcll
+    // replicate filter using d3 method -> d3 object of filters
+    return valuesArray.filter(e =>e.toLowerCase().includes(input.toLowerCase()));
+   }
+
+  function getValue(val){
+    
+    // if no value, have an empty page, 
+    if(!val){
+      result.innerHTML='';
+      return
+    }
   
-     result.innerHTML = res;
+    // search goes here 
+    let data = autoComplete(val);
+   
+   // append list data
+    let res = '';
+    data.forEach(e=>{
+       res += '<li>'+e+'</li>';
+       console.log('hi');
+    })
 
-     // Result
-     console.log('The components:~!!!', result); // Holds the name of the variables in a <li><li>
+ 
+    result.innerHTML = res;
+
+    // Result
+    //console.log('The components:~!!!', result); // Holds the name of the variables in a <li><li>
 
 
- }
+  } 
 
+  let searchInput = document.getElementById('searchInput');
+  searchInput.addEventListener('keyup', () => {
+    const searchInput: HTMLInputElement = document.getElementById('searchInput') as HTMLInputElement
+    const value: string = searchInput.value
+
+    getValue(value)
+  })
+}
 
 
 // export { getValue }
-export default getValue;
+export default addSearchListener;

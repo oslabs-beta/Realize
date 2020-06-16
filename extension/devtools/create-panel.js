93d3b5101eb3eec18b9993ddcaeac3e94360dcad
Henry Black <blackhaj@gmail.com>
2020-06-16 18:14:11 -0500

    Removed redundant code

--- extension/devtools/create-panel.js
+++ extension/devtools/create-panel.js
@@ -6,11 +6,4 @@ chrome.devtools.panels.create(
   '', // icon
   './panel.html' // content
 );
-// Adds a sidebar to the elements panel
-// Not sure if useful and only looks like it can be added to Elements (i.e. not our own panel)
-// See Devtools for more information - https://developer.chrome.com/extensions/devtools
-// chrome.devtools.panels.elements.createSidebarPane("My Sidebar",
-//     function(sidebar) {
-//         // sidebar initialization code here
-//         sidebar.setObject({ some_data: "Some data to show" });
-// });
+
